2630bc41da3ca3ca94f1de76f7f178ca8ffeeee5
Elliott Gilliland <mxgilliland@students.everettcc.edu>
2025-12-08 22:09:27 +0000

    mostly done. mostly

--- madness_functions.py
+++ madness_functions.py
@@ -1,48 +1,86 @@
 import random
 
-# sheriff - .38 mag 2d4
-
+# Provide a reminder that the user needs a weapon to proceed to the end
 def weapon_reminder():
     print("Remember, you must have at least one weapon to proceed to the end.")
 
+# Provide a weapon choice for fights
 def weapon_choice():
-    print("You are under attack! What weapon will you pick?")
-    print()
+    print("You are under attack!")
+    print(["Katana", "9mm", "Deagle", "Random Attack"])
+    weapon_choice_input = input("What weapon will you pick? ")
+    if weapon_choice_input == "katana":
+        katana()
+    else:
+        if weapon_choice_input == "9mm":
+            ninemm()
+        else:
+            if weapon_choice_input == "deagle":
+                deagle()
+            else:
+                if weapon_choice_input == "random attack":
+                    randomized_weapon()
+                else:
+                    print("Invalid option.")
 
+# Give the user a katana option, 1d12
 def katana():
     print("You pull out the katana and lunge forward.")
     katana_attack = random.randint(1, 12)
     if katana_attack <= 2:
         print("You stumble as you land your attack, making clumsy work with your katana. You deal " + str(katana_attack) + " damage.")
+    else:
+        if katana_attack <= 6:
+            print("You manage to land your attack, slicing your opponent through the arm. You deal " + str(katana_attack) + " damage.")
+        else:
+            if katana_attack <= 10:
+                print("You land a critical hit! You deal " + str(katana_attack) + " damage.")
 
+# Give the user a 9mm option, 1d8
 def ninemm():
     print("You pull out your 9mm and do your best to aim and fire.")
     ninemm_attack = random.randint(1, 8)
-    if ninemm_attack <= 4:
-        print("")
+    if ninemm_attack <= 2:
+        print("You fire your 9mm, but the bullet barely grazes him. You deal " + str(ninemm_attack) + " damage.")
+    else:
+        if ninemm_attack <= 4:
+            print("Neutral hit "+ str(ninemm_attack) + " damage.")
+        else:
+            if ninemm_attack <= 6:
+                print("Critical hit "+ str(ninemm_attack) + " damage.")
 
+# Give the user a deagle option, 1d12
 def deagle():
     print("You pull out a pistol, aim, and fire at the Sheriff.")
     deagle_attack = random.randint(1, 12)
     if deagle_attack <= 2:
-        print("")
+        print("Barely hit " + str(deagle_attack) + " damage.")
+    else:
+        if deagle_attack <= 6:
+            print("Neutral hit " + str(deagle_attack) + " damage.")
+        else:
+            if deagle_attack <= 10:
+                print("Critical hit " + str(deagle_attack) + " damage.")
 
+# Provide the user with a randomize option, these filter through the other attack options and will do damage accordingly
 def randomized_weapon():
-    random.choice([katana, ninemm, deagle])
-    if katana:
+    randomized_weapon_choice = random.choice([katana, ninemm, deagle])
+    if randomized_weapon_choice == katana:
         katana()
-    if ninemm:
+    if randomized_weapon_choice == ninemm:
         ninemm()
-    if deagle:
+    if randomized_weapon_choice == deagle:
         deagle()
 
+# Sheriff's attack is 2d4 defined below
 def sheriff():
     print("The Sheriff pulls out his dual .38 magnums, and tries to aim and shoot at you.")
     sheriff_attack = random.randint(1, 4) + random.randint(1, 4)
     if sheriff_attack <= 2:
         print("He fires his guns in your direction, and the bullets graze your arm, but don't hit anywhere vital. You take " + str(sheriff_attack) + " damage.")
-
-
-#def damage():
-#    damage_taken = random.randint(1, 4) + random.randint(1, 4)
-#    print("You take " + str(damage_taken) + " damage and recoil as you make your next move.")
+    else:
+        if sheriff_attack <= 4:
+            print("He fires his guns at you, and the bullets graze your torso. You take " + str(sheriff_attack) + " damage.")
+        else:
+            if sheriff_attack <= 6:
+                print("He fires his guns, and the bullets hit your chest. You take " + str(sheriff_attack) + " damage.")
--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from madness_functions import *
 
 # NOTES TO SELF
 # FIND A WAY TO TRACK HEALTH. some sort of loop is good for this i imagine
+# the health tracker will probably be a for loop. don't forget to implement this
+
 # Create a list for the weapon "inventory", won't actually be a proper inventory but will be called upon
 # Figure out health stats for Hank + the Sheriff
-# Time.sleep will be used
-
 weapon_list = [] # will eventually contain a katana, deagle, and 9mm
 hank_health = 54 # Pulled from the MADNESS: Project Nexus wiki for Hank's health stats
 sheriff_health = 75 # Pulled from the MADNESS: Project Nexus wiki for Sheriff's health stats
 
 # The way the game starts is with Hank waking up in a dream
 # When they wake up, they get up off the ground, look around
+print("There's a loud bang outside as you open your eyes.")
+time.sleep(.5)
+print("You shake off sleep with ease, and look down at your hands.")
+time.sleep(.5)
+print("Your hands are gloved, though your gloves have been long stained with blood.")
+time.sleep(1)
 look_around = input("Would you like to look around? ")
-# this is botched, fix this later and reference the program with if homework to find the solution
-# can also reference the elif solutions, there is a picture on your phone to look at
 if look_around == "Yes":
     print("You get up and look around. On the table nearby, you see a katana lying around.")
     weapon_reminder()
@@ -29,54 +33,74 @@ if look_around == "Yes":
         print("You leave the katana behind, you'll find something else, you're sure.")
 else:
     print("You decide not to look around.")
-# input an else statement that passes over the katana options, don't forget to include this above
-
 # Here, there will be a pause after they take in their surroundings
 time.sleep (1.5)
 # After this, they will have an opportunity to move around and explore
-print("After you finish looking around, you see that there's two different ways that you can go, forward or left.")
-cross_roads_one = input("Would you like to go forward or left? ")
-# this is botched, fix this later and reference the program with if homework to find the solution
-# can also reference the elif solutions, there is a picture on your phone to look at
-if cross_roads_one == "Forward" or "Left":
-    if cross_roads_one == "Forward":
-        print("You proceed forward, and find a small room tucked away.")
-        # deagle found in this room
-        time.sleep(1)
-        print("As you enter the room, you notice a deagle lying around on the floor.")
-        weapon_reminder()
-        deagle_pick_up = input("Would you like to pick up the deagle? ")
-        if deagle_pick_up == "Yes":
-            print("You pick up the deagle, and tuck it away into your holster.")
-            weapon_list.append("Deagle")
-            print(weapon_list)
-        else:
-            print("You leave the deagle behind, you'll find something else, you're sure.")
-        # if the user proceeds forward, they will need to turn around to leave and proceed
-    else:
-        if cross_roads_one == "Left":
-            print("You proceed to the room to the left.")
-        else:
-            print("Invalid direction.")
+print("After you finish looking around, you see that there's a room tucked away in front of you. You decide to check it out.")
+print("You proceed forward, and enter the storage room.")
+# deagle found in this room
+time.sleep(1)
+print("As you enter the room, you notice a deagle lying around on the floor.")
+weapon_reminder()
+deagle_pick_up = input("Would you like to pick up the deagle? ")
+if deagle_pick_up == "Yes":
+    print("You pick up the deagle, and tuck it away into your holster.")
+    weapon_list.append("Deagle")
+    print(weapon_list)
+    print("After you pick up the deagle, you smile to yourself. Your power grows.")
+else:
+    print("You leave the deagle behind, you'll find something else, you're sure.")
+print("After you're done exploring, you turn around and return to the room you started in.")
+print("There's another room to your left as you return to the room you started in. You decide it would be best to check out this next room.")
     # no weapons found here, but will have an opportunity to spy on the sheriff
-    # next room is forward from here
-    # 3rd room will contain 9mm
-    # weapon_reminder()
-    # 4th room is to the right of 3rd room
-    # 5th room is forward of 4th room + where the boss fight will occur
-# This is where they will have the chance to pick up weapons as they explore
+print("There's nothing that catches your eye as you enter this room. You let out an irritated sigh.")
+time.sleep(1)
+print("However...")
+print("As you look around, you notice a window. This window exposes a room nearby.")
+print("The exposed room reveals your target, the Sheriff. You would grin if you could.")
+print("You decide to spy on him for a bit before continuing through the rooms.")
+print("You make your way to the next room, and look around.")
+print("As you're looking around, something catches your eye.")
+weapon_reminder()
+ninemm_pickup = input("It's a 9mm! Would you like to pick it up? ")
+if ninemm_pickup == "Yes":
+    print("You pick up the 9mm, and tuck it away into your other holster.")
+    weapon_list.append("9mm")
+    print(weapon_list)
+    print("After you pick up the pistol, you continue looking around.")
+else:
+    print("You leave the 9mm behind. Hopefully you've already picked something else up by now.")
+print("After you finish looking around, a vent catches your eye.")
+print("You take the grate off of the vent, and pull yourself up into it.")
+time.sleep(1)
 # They will have to move through a few rooms, stalking to follow the Sheriff stealthily through a building.
-# This will consist of a decent amount of story lines explaining what the Sheriff is doing and how Hank is watching him
-
-# What the boss fight will look like
+print("You peer below as you make your way through the vent system, seeing the Sheriff talking to two people.")
+print("You can't quite make out what they're saying, but it's not like you care in the first place.")
+time.sleep(1)
+print("You roll your eyes, and carry on with your mission. You're almost there, you're sure of it.")
+time.sleep(3)
+print("The ventilation system leads into another room, and you peer through the grate to see what's below.")
+print("There he is.")
+print("Your target. The Sheriff.")
+time.sleep(2)
 # When you enter the final room, it will trigger an encounter with the Sheriff
+print("As you drop into the room from the vents, the Sheriff looks like he's about to jump out of his skin as you startle him.")
+print("The Sheriff takes a moment to collect himself, and takes a deep breath.")
 # The Sheriff will start monologuing, Hank won't care much
-    # print statements of sheriff's monologuing
+print("After having done so, the Sheriff looks in your direction, and starts speaking.")
+time.sleep(3)
+print("As the Sheriff goes on about his monologue, you roll your eyes impatiently. You just want him to be done talking already.")
 # Once the Sheriff's monologue is over, the fight begins
 # This is where loops and the RNG will come into play
-    # while loop will go here
-# There will also be an option to select a weapon from the user's inventory, this will be a list
-    # weapon_choice()
+while (sheriff_health > 0) or (hank_health > 0):
+    # There will also be an option to select a weapon from the user's inventory, this will be a list
+    weapon_choice()
+    time.sleep(1)
+    sheriff()
+    if sheriff_health <= 0:
+        print("The Sheriff staggers to the ground, trying to get in enough oxygen. You tower over him. You've won.")
+    if hank_health <= 0:
+        print("The Sheriff shoots you down. Game over.")
 # The weapon chosen will have a specific range for a number to be generated, which will be the damage done to the Sheriff
 # The Sheriff will also have specific RNG for his weapon.
 # There is a chance that the player can lose based on the RNG. It will not always happen that the player will win.
